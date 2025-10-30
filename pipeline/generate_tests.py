d3bcb21bd1a68048a65aec8ec5f03238dcbeee4d
Stephen MacVicar <smacvicar@mitre.org>
2025-10-30 12:57:52 -0400

    remove unused imports from generate_tests script

--- pipeline/generate_tests.py
+++ pipeline/generate_tests.py
@@ -1,14 +1,6 @@
-import inspect
-import json
 import llm_utils
-import importlib
 import os
 import test_kit_07
-import tiktoken
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
-from glob import glob
 
 llm_clients = llm_utils.LLMApiClient()
 
@@ -16,12 +8,12 @@ working_directory = os.getcwd()
 
 base_artifacts_path = "../artifacts-demo"
 
-# Faster generation- no LLM self evaluation
+# TODO: add command line param for self evaluation
 test_kit_07.generate_inferno_test_kit(
-    client_instance=llm_clients, #initialize llm clients
-    api_type='claude',  #set API
+    client_instance=llm_clients,
+    api_type='claude',
     test_plan_file=os.path.join(working_directory, base_artifacts_path, "test_plan", "test_plan.md"),
     ig_name='US Core',
     output_dir=os.path.join(working_directory, base_artifacts_path, "tests"),
-    enable_validation=False  #disable LLM self evaluation
+    enable_validation=False
 )
