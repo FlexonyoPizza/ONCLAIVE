18b447bd291b39e7b60c8e6fbddc9f620505e0b2
Stephen MacVicar <smacvicar@mitre.org>
2025-11-06 11:46:17 -0500

    add command line arguments to ig processing

--- pipeline/process_ig.py
+++ pipeline/process_ig.py
@@ -1,15 +1,48 @@
+import argparse
 import html_narrative_extractor_01
 import markdown_cleaner_02
 import os
 
 working_directory = os.getcwd()
 
-base_artifacts_path = "../artifacts-demo"
+demo_artifacts_path = "demo-artifacts"
+
+parser = argparse.ArgumentParser(
+    description="Convert html IG files into cleaned markdown files"
+)
+parser.add_argument(
+    'artifacts_dir',
+    default=demo_artifacts_path,
+    help="Relative path to the base artifacts directory"
+)
+
+parser.add_argument(
+    '-v', '--verbose',
+    default=False,
+    type=bool,
+    help="Enable verbose logging"
+)
+
+parser.add_argument(
+    '-e', '--exclude-pattern',
+    action='append',
+    help="Files matching regular expressions in this argument will be ignored"
+)
+
+args = parser.parse_args()
+
+relative_artifacts_dir = args.artifacts_dir
+verbose = args.verbose
+exclude_patterns = args.exclude_pattern
+
+final_artifacts_dir = os.path.abspath(os.path.join(working_directory, relative_artifacts_dir))
 
 result = html_narrative_extractor_01.convert_local_html_to_markdown(
-    artifacts_dir=os.path.join(working_directory, base_artifacts_path)
+    artifacts_dir=final_artifacts_dir,
+    verbose=verbose,
+    exclude_patterns=exclude_patterns
 )
 
 markdown_cleaner_02.process_directory(
-    artifacts_dir=os.path.join(working_directory, base_artifacts_path)
+    artifacts_dir=final_artifacts_dir
 )
